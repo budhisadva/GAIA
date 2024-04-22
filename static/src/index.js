8a86916581bebe4632f170a9ce0fd6bc8c5a8b4b
Gabriel <budhisadva@gmail.com>
2024-04-22 12:11:28 -0600

    Navegacion

--- static/src/index.js
+++ static/src/index.js
@@ -1,1 +1,11 @@
-console.log(`Ancho: ${window.innerWidth}, Alto: ${window.innerHeight}`);
+const opcionesListener = () => {
+  let perfil = [...document.getElementsByClassName('perfil__nombre')][0];
+  perfil.addEventListener('click', (e) => {
+    let opciones = [...document.getElementsByClassName('opciones')][0];
+    opciones.toggleAttribute('hidden');
+  });
+}
+
+window.onload = () => {
+  opcionesListener();
+}
