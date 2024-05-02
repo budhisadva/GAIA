c29efb31650ff6b8a5c88e5b4dc07b4c6c3d79eb
Alonsopro <alonso_togo@hotmail.com>
2024-05-02 11:30:11 -0600

    modifique publicar

--- static/src/VerActividad/funciones.js
+++ static/src/VerActividad/funciones.js
@@ -1,6 +1,7 @@
 const listarPosts = (contenedor) => {
   console.log(contenedor);
-  contenedor.innerHTML = 'Hola mundo';
+contenedor.innerHTML = 'Hola mundo';
+contenedor.innerHTML = 'rama alonso ';
 }
 
 export { listarPosts };
