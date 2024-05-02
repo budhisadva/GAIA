c0d1085e445dee2f17b1f716f138430e1fe78a3c
Etzael Hedding <etzael@ciencias.unam.mx>
2024-05-01 18:51:02 -0600

    Fallos autenticacion usuario

--- IniciarSesion/views.py
+++ IniciarSesion/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import Usuario
 
-# Create your views here.
+# login_usuario
 def index(request):
-    return render(request, 'IniciarSesion.html')
+    if request.method == "POST":
+        email = request.POST['correo']
+        password = request.POST['contrasena']
+        user = authenticate(request, correo=email, contrasena=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Se ha iniciado la sesion"))
+            return redirect('/verActividad')
+        else:
+            messages.success(request, ("Hubo un error al iniciar sesion. Intente nuevamente"))
+            return redirect('/publicar')
+    else:
+        return render(request, 'IniciarSesion.html')
+
+def logout_usuario(request):
+    logout(request)
+    messages.success(request, ("Se ha cerrado la sesion"))
+    return redirect('IniciarSesion.html')
