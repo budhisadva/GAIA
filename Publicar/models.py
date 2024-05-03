1126000fb390587888fef93e944e83ef8cb244f8
Alonsopro <alonso_togo@hotmail.com>
2024-05-03 13:06:12 -0600

    Publicar ya guarda texto y imagen en la base de datos

--- Publicar/models.py
+++ Publicar/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class PublicarForm(models.Model):
     #guardo el texto 
     texto = models.TextField(max_length=500)
-   # foto = models.ImageField(upload_to='fotos/', blank=True, null=True)
-    #video = models.URLField(blank=True, null=True)
+    foto = models.ImageField(upload_to='fotosPost/', blank=True, null=True)
+    video = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.texto
--- Publicar/views.py
+++ Publicar/views.py
@@ -5,9 +5,20 @@ from Publicar.models import PublicarForm # modelo donde guardare la publicacion
 
 def publicar(request):
     print("Aqui voy a guardar las publicaciones ")
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        texto = request.POST['texto']
+        foto = request.FILES.get('foto')
+        publicacion = PublicarForm(texto=texto, foto=foto)
+        publicacion.save()
+        if foto:
+            print("Nombre de la foto:", foto.name)
+            print("Tipo de archivo:", foto.content_type)
+            print("Tamaño de la foto:", foto.size)
+
     print (request.POST['texto'])
-    publicacion = PublicarForm(texto=request.POST['texto'])
-    publicacion.save()
+    #publicacion = PublicarForm(texto=request.POST['texto'])
+    #publicacion.save()
     # regreso a ver actividad despues de publicar 
     return HttpResponseRedirect('/verActividad')
 # Create your views here.
