65fc75ff0efa000d57622ad9f128af2a1ac29daa
Gabriel <budhisadva@gmail.com>
2024-05-19 15:17:08 -0600

    configure para el backend

--- IniciarSesion/models.py
+++ IniciarSesion/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=255)
-    correo = models.EmailField()
--- Publicar/views.py
+++ Publicar/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Publicar')
+def crear(request):
+    formulario = request.POST.dict()
+    print(formulario)
+    return HttpResponseRedirect('/verActividad')
