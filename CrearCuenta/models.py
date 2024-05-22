00ce8ac7d2e2a2325a8cdde8d3e1827dbbcfaa42
Gabriel <budhisadva@gmail.com>
2024-05-22 13:35:40 -0600

    Termine: crearCuenta

--- CrearCuenta/models.py
+++ CrearCuenta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    genero = models.CharField(max_length=10)
+    fechaN = models.DateField()
--- CrearCuenta/views.py
+++ CrearCuenta/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from . import models
 
 # Create your views here.
 def index(request):
     return render(request, 'CrearCuenta.html')
 
 def crearCuenta(request):
-    formulario = request.POST.dict()
-    print(formulario)
-    return HttpResponseRedirect('/verActividad')
+    if request.method == 'POST':
+        formulario = request.POST.dict()
+        try:
+            user = User.objects.create_user(username=formulario['nombre'],
+                                            email=formulario['correo'],
+                                            password=formulario['contrasena'])
+            user.save()
+            login(request, user)
+            usuario = models.Usuario(usuario=user,
+                                     genero=formulario['genero'],
+                                     fechaN=formulario['fecha'])
+            usuario.save()
+            return redirect('VerActividad:index')
+        except Exception as e:
+            return render(request, 'CrearCuenta.html', {
+                'error': f'Error: {e}'
+            })
