7134e4e4a8dbae5f1f4c196db47c9f34f2f2d5df
Gabriel <budhisadva@gmail.com>
2024-05-24 01:23:26 -0600

    Avance: publicar

--- Publicar/models.py
+++ Publicar/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     texto = models.CharField(max_length=500)
-    imagen = models.ImageField(default='null', upload_to="post")
+    imagen = models.ImageField(default='null', upload_to='posts')
     enlace = models.CharField(max_length=200, default='null')
     fecha_creacion = models.DateTimeField(auto_now_add=True)
--- Publicar/views.py
+++ Publicar/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
 
-# Create your views here.
+@login_required
 def crear(request):
-    formulario = request.POST.dict()
-    print(formulario)
-    return HttpResponseRedirect('/verActividad')
+    if request.method == 'POST':
+        if request.POST['texto']:
+            post = Post(
+                usuario = request.user,
+                texto = request.POST['texto']
+            )
+            post.save()
+            if request.POST['enlace']:
+                post.enlace = request.POST['enlace']
+                post.save()
+            if request.FILES['foto']:
+                post.imagen = request.FILES['foto']
+                post.save()
+    return render(request, 'VerActividad.html')
