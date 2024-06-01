54ee90a21a692f47c8c1233d13cc6d99d66558d3
Gabriel <budhisadva@gmail.com>
2024-05-31 23:53:26 -0600

    Quite la excepcion en Publicar y cambie modelo

--- Publicar/models.py
+++ Publicar/models.py
@@ -6,5 +6,5 @@ class Post(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     texto = models.CharField(max_length=500)
     imagen = models.ImageField(default='null', upload_to='posts')
-    enlace = models.CharField(max_length=200, default='null')
+    enlace = models.URLField(max_length=200, default='null')
     fecha_creacion = models.DateTimeField(auto_now_add=True)
