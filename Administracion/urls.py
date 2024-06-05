8101bd77a703312a361f9b37f07a00ed51f5328a
Gabriel <budhisadva@gmail.com>
2024-06-05 06:05:25 -0600

    Termine: Administracion

--- Administracion/urls.py
+++ Administracion/urls.py
@@ -5,4 +5,7 @@ app_name = 'Administracion'
 
 urlpatterns = [
     path('base/', views.base, name='base'),
+    path('bloquear/<int:usr_id>/', views.bloquear, name='bloquear'),
+    path('desbloquear/<int:usr_id>/', views.desbloquear, name='desbloquear'),
+    path('eliminar/<int:usr_id>/', views.eliminar, name='eliminar'),
 ]
--- Administracion/views.py
+++ Administracion/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 # Create your views here.
 def base(request):
     users = User.objects.all().exclude(id=request.user.id)
-    print(users)
     return render(request, 'Administracion.html', {
         'usuarios': users
     })
+
+def bloquear(request, usr_id):
+    user = User.objects.get(id=usr_id)
+    user.is_active = False
+    user.save()
+    return redirect('Administracion:base')
+
+def desbloquear(request, usr_id):
+    user = User.objects.get(id=usr_id)
+    user.is_active = True
+    user.save()
+    return redirect('Administracion:base')
+
+def eliminar(request, usr_id):
+    user = User.objects.get(id=usr_id)
+    user.delete()
+    return redirect('Administracion:base')
